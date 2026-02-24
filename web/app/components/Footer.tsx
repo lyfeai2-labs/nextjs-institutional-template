a4891a1ff3a6f414f1b45c41f24f6663fa3edeaf
LyFeai2-Luiz Fernando-JuDô <lyfeai2@gmail.com>
2026-02-24 01:53:24 -0300

    Create Footer.tsx

---
+++ web/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="border-t border-gray-200 py-6 text-center text-sm text-gray-700">
+      <p>Observatório Cultural de Sergipe &copy; 2026</p>
+      <p>Contato: contato@observatorio-sergipe.org</p>
+      <p>Siga-nos nas redes: Instagram | Facebook</p>
+    </footer>
+  );
+}
